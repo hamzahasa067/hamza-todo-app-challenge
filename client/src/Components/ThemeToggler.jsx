7adcffff7f195bb63f7619c44b708fb7950cf503
Hamza Hasanain <53662962+hamzahasa067@users.noreply.github.com>
2022-11-09 18:58:09 +0200

    added theme toggle

--- client/src/Components/ThemeToggler.jsx
+++ client/src/Components/ThemeToggler.jsx
@@ -1,5 +1,16 @@
-import React from "react"
-
+import React, { useContext } from "react"
+import { ThemeContext, ToggleThemeContext } from "../contexts/themeContext"
+import LightButton from "../images/icon-sun.svg"
+import DarkButton from "../images/icon-moon.svg"
 export default function ThemeToggler() {
-  return <div>ThemeToggler</div>
+  const theme = useContext(ThemeContext)
+  const toggleTheme = useContext(ToggleThemeContext)
+  return (
+    <div className="theme-toggler">
+      <img
+        src={theme === "light" ? DarkButton : LightButton}
+        onClick={(_) => toggleTheme()}
+      />
+    </div>
+  )
 }
